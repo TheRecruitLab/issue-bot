ad233f94b20d78195d8fb59042bf5220ac7b9944
Dylan Flint <df@buildmidwestern.com>
2024-02-01 16:37:07 -0600

    [feat] Modified github action

--- index.js
+++ index.js
@@ -2,20 +2,35 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 
 async function run() {
-    const myToken = core.getInput('myToken');
-    const issueNumber = core.getInput('issue_number');
+    const githubToken = core.getInput('github_token');
+    const pullNumber = core.getInput('pull_number');
     const state = core.getInput('state');
 
-    const octokit = github.getOctokit(myToken)
+    const octokit = github.getOctokit(githubToken)
     const context = github.context;
 
-    const { data: pullRequest } = await octokit.rest.issues.update({
+    const { data: pullRequest } = await octokit.rest.pulls.get({
       ...context,
-      issue_number: issueNumber,
-      state,
+      pull_number: pullNumber,
     });
 
     console.log(pullRequest);
+
+    // if (! pullRequest) {
+    //   throw new Error('Pull Request could not be found.');
+    // }
+
+    const { data: issue } = await octokit.rest.issues.update({
+      ...context,
+      issue_number: pullRequest.issue_number,
+      state,
+    });
+
+    // if (! issue) {
+    //   throw new Error('An issue attached to the Pull Request could not be found.');
+    // }
+
+    console.log(`#${issue?.issue_number} was changed to ${issue.state}`);
 }
 
 run();
