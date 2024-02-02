a78376641f54f4b39f1101e5336d68629e4bfe27
Dylan Flint <df@buildmidwestern.com>
2024-02-02 11:44:56 -0600

    [feat] Modified github action

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
 
     const { repository } = await graphqlWithAuth(`
       {
-        repository(owner: "octokit", name: "graphql.js") {
+        repository(owner: ${context.payload?.repository?.owner?.login}, name: ${context.payload?.repository?.name}) {
           pullRequest(number: ${context.payload.pull_request.number}) { 
             author {
               login
