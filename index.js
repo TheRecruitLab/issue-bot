b97b1cdd589337ad50255141e819aef00286692d
Dylan Flint <df@buildmidwestern.com>
2024-02-02 12:43:19 -0600

    [feat] Added integration to modify the issue state

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
-const { graphql } = require("@octokit/graphql");
+  const github = require('@actions/github');
+  const core = require('@actions/core');
+  const { graphql } = require("@octokit/graphql");
 
-async function run() {
+  async function run() {
     const githubToken = core.getInput('github_token');
     const state = core.getInput('state');
 
     const octokit = github.getOctokit(githubToken)
     const context = github.context;
+
+    const owner = context.payload?.repository?.owner?.login;
+    const repo = context.payload?.repository?.name;
 
     const graphqlWithAuth = graphql.defaults({
       headers: {
@@ -21,7 +24,7 @@ async function run() {
 
     const { repository } = await graphqlWithAuth(`
       {
-        repository(owner: "${context.payload?.repository?.owner?.login}", name: "${context.payload?.repository?.name}") {
+        repository(owner: "${owner}", name: "${repo}") {
           pullRequest(number: ${context.payload.pull_request.number}) { 
             author {
               login
@@ -40,28 +43,21 @@ async function run() {
       }
     `);
 
-console.log({
-  PR: repository.pullRequest,
-  closing: repository.pullRequest.closingIssuesReferences.nodes[0],
-});
+    const linkedIssues = repository?.pullRequest?.closingIssuesReferences?.nodes || [];
 
-    // if (! pullRequest) {
-    //   throw new Error('Pull Request could not be found.');
-    // } else if (! pullRequest?.issue_number) {
-    //   console.log('Pull Request is linked to an existing issue.');
-    // }
+    for (const linkedIssue of linkedIssues) {
+      const { data: issue } = await octokit.rest.issues.update({
+        ...context,
+        issue_number: linkedIssue.number,
+        state,
+      });
 
-    // const { data: issue } = await octokit.rest.issues.update({
-    //   ...context,
-    //   issue_number: pullRequest.issue_number,
-    //   state,
-    // });
+      if (! issue) {
+        throw new Error('An issue attached to the Pull Request could not be found.');
+      }
 
-    // if (! issue) {
-    //   throw new Error('An issue attached to the Pull Request could not be found.');
-    // }
-
-    // console.log(`#${issue?.issue_number} was changed to ${issue.state}`);
+      console.log(`#${linkedIssue.number} was changed to ${issue.state}`);
+    }
 }
 
 run();
