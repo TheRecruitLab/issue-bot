b23709530ec6a263b1eeacdfb38266840ef24e90
Dylan Flint <df@buildmidwestern.com>
2024-02-02 10:19:00 -0600

    [feat] Modified github action

--- index.js
+++ index.js
@@ -16,23 +16,28 @@ async function run() {
       state,
     });
 
-    const { data: pullRequest } = await octokit.rest.pulls.get({
-      repo: context.payload.sender.repository.name,
-      owner: context.payload.sender.owner.name,
-      pull_number: pullNumber,
+    console.log({
+      github: github.context.payload.pull_request,
+      sender: context?.payload?.sender,
     });
 
-    console.log(pullRequest);
+    // const { data: pullRequest } = await octokit.rest.pulls.get({
+    //   repo: context.payload.sender.repository.name,
+    //   owner: context.payload.sender.owner.name,
+    //   pull_number: pullNumber,
+    // });
+
+    // console.log(pullRequest);
 
     // if (! pullRequest) {
     //   throw new Error('Pull Request could not be found.');
     // }
 
-    const { data: issue } = await octokit.rest.issues.update({
-      ...context,
-      issue_number: pullRequest.issue_number,
-      state,
-    });
+    // const { data: issue } = await octokit.rest.issues.update({
+    //   ...context,
+    //   issue_number: pullRequest.issue_number,
+    //   state,
+    // });
 
     // if (! issue) {
     //   throw new Error('An issue attached to the Pull Request could not be found.');
