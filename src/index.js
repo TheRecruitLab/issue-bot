f2e4a66c9d11d195a941da4faf98cd73fb099b77
Dylan Flint <df@buildmidwestern.com>
2024-03-08 10:57:55 -0600

    [wip] Added logging for testing graphql custom field associations

--- src/index.js
+++ src/index.js
@@ -1,72 +1,109 @@
-  const github = require('@actions/github');
-  const core = require('@actions/core');
-  const { graphql } = require("@octokit/graphql");
+const github = require('@actions/github');
+const core = require('@actions/core');
+const { graphql } = require("@octokit/graphql");
 
-  async function run() {
-    const githubToken = core.getInput('github_token');
-    const state = core.getInput('state');
-    const requiresMerge = Boolean(core.getInput('requires_merge')) === 'true';
+async function run() {
+  const githubToken = core.getInput('github_token');
+  const state = core.getInput('state');
+  const requiresMerge = Boolean(core.getInput('requires_merge')) === 'true';
 
-    const octokit = github.getOctokit(githubToken)
-    const context = github.context;
+  const octokit = github.getOctokit(githubToken)
+  const context = github.context;
 
-    const owner = context.payload?.repository?.owner?.login;
-    const repo = context.payload?.repository?.name;
+  const owner = context.payload?.repository?.owner?.login;
+  const repo = context.payload?.repository?.name;
 
-    const graphqlWithAuth = graphql.defaults({
-      headers: {
-        authorization: `token ${githubToken}`,
-      },
-    });
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token ${githubToken}`,
+    },
+  });
 
-    if (! context?.payload?.pull_request) {
-        throw new Error('Invalid Github event. Must be a pull_request event.');
-    }
+  if (! context?.payload?.pull_request) {
+      throw new Error('Invalid Github event. Must be a pull_request event.');
+  }
 
-    const { repository } = await graphqlWithAuth(`
-      {
-        repository(owner: "${owner}", name: "${repo}") {
-          pullRequest(number: ${context.payload.pull_request.number}) {
-            author {
-              login
+  const { repository } = await graphqlWithAuth(`
+    {
+      repository(owner: "${owner}", name: "${repo}") {
+        pullRequest(number: ${context.payload.pull_request.number}) {
+          author {
+            login
+          },
+          closingIssuesReferences(first: 100) { 
+            nodes { 
+              number,
+              projectsV2(first: 100) {
+                nodes {
+                  id,
+                  fields(first: 100) {
+                    nodes {
+                      id,
+                      name
+                    }
+                  }
+                }
+              }
             },
-            closingIssuesReferences(first: 100) { 
-              nodes { 
-                number 
-              },
-              totalCount,
-            },
-            id,
-            merged,
-            number,
-            state,
-          }
+            totalCount,
+          },
+          id,
+          merged,
+          number,
+          state,
         }
       }
-    `);
+    }
+  `);
 
-    if (requiresMerge && ! repository?.pullRequest?.merged) {
-      console.log('The Pull Request must be merged in order to update associated issues.');
-      
-      return;
+  if (requiresMerge && ! repository?.pullRequest?.merged) {
+    console.log('The Pull Request must be merged in order to update associated issues.');
+    
+    return;
+  }
+
+  const linkedIssues = repository?.pullRequest?.closingIssuesReferences?.nodes || [];
+
+  for (const linkedIssue of linkedIssues) {
+    const { data: issue } = await octokit.rest.issues.update({
+      repo,
+      owner,
+      issue_number: linkedIssue.number,
+      state,
+    });
+
+    if (! issue) {
+      throw new Error('An issue attached to the Pull Request could not be found.');
     }
 
-    const linkedIssues = repository?.pullRequest?.closingIssuesReferences?.nodes || [];
-
-    for (const linkedIssue of linkedIssues) {
-      const { data: issue } = await octokit.rest.issues.update({
-        repo,
-        owner,
-        issue_number: linkedIssue.number,
-        state,
-      });
-
-      if (! issue) {
-        throw new Error('An issue attached to the Pull Request could not be found.');
+    for (const project of (issue?.projectsV2?.nodes || [])) {
+      console.log('PROJECT ID', project?.id);
+      for (const field of (project?.fields?.nodes || [])) {
+        console.log('FIELD', { ...field });
       }
-
-      console.log(`#${linkedIssue.number} was changed to ${issue.state}`);
     }
+
+  //   await graphqlWithAuth(`
+  //   mutation {
+  //     updateProjectV2ItemFieldValue(
+  //       input: {
+  //         projectId: "PROJECT_ID"
+  //         itemId: "ITEM_ID"
+  //         fieldId: "FIELD_ID"
+  //         value: { 
+  //           singleSelectOptionId: "OPTION_ID"        
+  //         }
+  //       }
+  //     ) {
+  //       projectV2Item {
+  //         id
+  //       }
+  //     }
+  //   }
+  // `);
+
+    console.log(`#${linkedIssue.number} was changed to ${issue.state}`);
+  }
 }
 
 run();
