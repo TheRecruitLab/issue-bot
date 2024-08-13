d3bf7c8ef412c24058585a131dfee3ba478ef5e1
Dylan Flint <df@buildmidwestern.com>
2024-08-13 10:56:35 -0500

    [wip] Testing actions

--- src/create-pr-if-out-of-sync.js
+++ src/create-pr-if-out-of-sync.js
@@ -35,10 +35,15 @@ function getContextVars() {
   };
 }
 
-async function getListOfPullRequests(octokit, { owner, repo }) {
+async function getListOfPullRequests(octokit, { owner, repo, base, state = 'open' }) {
   const pullRequests = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
     owner,
     repo,
+    base,
+    state,
+    per_page: 100,
+    sort: 'created',
+    direction: 'desc',
   });
 
   return pullRequests.data.map(({ base, head }) => ({ base, head }));
