00c6d8f40ea725ee1c2562c83f911834d0e1f470
Dylan Flint <df@buildmidwestern.com>
2024-08-13 16:00:40 -0500

    [feat] Reverted api folder and fixed data parsing in chunked requests

--- src/api/updatePullRequest.js
+++
@@ -1,8 +0,0 @@
-export async function updatePullRequest({ octokit, owner, repo, pull_number, ...rest }) {
-  const response = await octokit.request('PATCH /repos/{owner}/{repo}/pulls/{pull_number}', {
-    owner,
-    repo,
-    pull_number,
-    ...rest,
-  });
-}
